71c1d3123bcd8b7dc78670d06b83a914e6fcef79
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-19 18:27:57 -0400

    init

---
+++ Counter/Counter.java
@@ -0,0 +1,12 @@
+package Counter;
+
+public class Counter {
+
+    public static void main(String[] args) {
+        int number = 0;
+        while (number < 100) {
+            number += 10;
+            System.out.println(number);
+        }
+    }
+}
